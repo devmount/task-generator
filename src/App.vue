067c12f27a44a1295162452480f8289598ff54bf
devmount <mail@devmount.de>
2018-11-06 00:48:45 +0100

    :heavy_plus_sign: implement task calculation

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,15 @@
               <h5>Zahlenbereich 1. Faktor / Divisor</h5>
               <div class="columns">
                 <div class="column col-6">
-                  <div class="form-group">
-                    <input class="form-input" :value="min1" type="number" placeholder="Kleinster Wert, z.B. 1">
+                  <div class="input-group" :class="{ 'has-error': error.min1 }">
+                    <span class="input-group-addon addon-lg">Von</span>
+                    <input class="form-input input-lg" v-model="min1" type="number" placeholder="z.B. 1">
                   </div>
                 </div>
                 <div class="column col-6">
-                  <div class="form-group">
-                    <input class="form-input" :value="max1" type="number" placeholder="Größter Wert, z.B. 10">
+                  <div class="input-group" :class="{ 'has-error': error.max1 }">
+                    <span class="input-group-addon addon-lg">Bis</span>
+                    <input class="form-input input-lg" v-model="max1" type="number" placeholder="z.B. 10">
                   </div>
                 </div>
               </div>
@@ -30,13 +32,15 @@
               <h5 class="mt-3">Zahlenbereich 2. Faktor / Quotient</h5>
               <div class="columns">
                 <div class="column col-6">
-                  <div class="form-group">
-                    <input class="form-input" :value="min2" type="number" placeholder="Kleinster Wert, z.B. 1">
+                  <div class="input-group" :class="{ 'has-error': error.min2 }">
+                    <span class="input-group-addon addon-lg">Von</span>
+                    <input class="form-input input-lg" v-model="min2" type="number" placeholder="z.B. 1">
                   </div>
                 </div>
                 <div class="column col-6">
-                  <div class="form-group">
-                    <input class="form-input" :value="max2" type="number" placeholder="Größter Wert, z.B. 10">
+                  <div class="input-group" :class="{ 'has-error': error.max2 }">
+                    <span class="input-group-addon addon-lg">Bis</span>
+                    <input class="form-input input-lg" v-model="max2" type="number" placeholder="z.B. 10">
                   </div>
                 </div>
               </div>
@@ -44,13 +48,13 @@
               <h5 class="mt-3">Anzahl der Aufgaben</h5>
               <div class="columns">
                 <div class="column col-6">
-                  <div class="form-group">
-                    <input class="form-input" :value="number" type="number" placeholder="Aufgabenanzahl, z.B. 20">
+                  <div class="form-group" :class="{ 'has-error': error.number }">
+                    <input class="form-input input-lg" v-model="number" type="number" placeholder="z.B. 20">
                   </div>
                 </div>
                 <div class="column col-6">
                   <div class="form-group">
-                    <button class="btn btn-primary float-right" @click="generateTasks">Aufgaben generieren <i class="icon icon-arrow-right"></i></button>
+                    <button class="btn btn-primary float-right btn-lg" @click="generateTasks">Start <i class="icon icon-arrow-right"></i></button>
                   </div>
                 </div>
               </div>
@@ -62,24 +66,42 @@
         <div class="column col-6">
           <h3 class="text-center">Ausgabe</h3>
 
-          <div class="card output">
+          <div class="empty output" v-if="!result">
+            <div class="empty-icon">
+              <i class="icon icon-3x icon-arrow-left"></i>
+            </div>
+            <p class="empty-title h5">Noch kein Ergebnis verfügbar</p>
+            <p class="empty-subtitle">Fülle das Formular aus, um Aufgaben zu generieren.</p>
+          </div>
+          <div class="card output" v-else>
             <div class="card-body">
-
               <div class="columns">
                 <div class="column col-6">
                   <h5>Multiplikation</h5>
-                  <ul>
-                    <li v-for="t in mtasks">{{ t }}</li>
+                  <ul id="mlist">
+                    <li v-for="t in mtasks" :key="t">{{ t }}</li>
                   </ul>
+                  <button class="btn clip" :class="{ 'btn-success': copied.mlist }" data-clipboard-target="#mlist" @click="copied.mlist = true; copied.dlist = false">
+                    <span v-if="!copied.mlist">Kopieren</span>
+                    <i v-else class="icon icon-check"></i>
+                  </button>
                 </div>
                 <div class="column col-6">
                   <h5>Division</h5>
-                  <ul>
-                    <li v-for="t in dtasks">{{ t }}</li>
+                  <ul id="dlist">
+                    <li v-for="t in dtasks" :key="t">{{ t }}</li>
                   </ul>
+                  <button class="btn clip" :class="{ 'btn-success': copied.dlist }" data-clipboard-target="#dlist" @click="copied.dlist = true; copied.mlist = false">
+                    <span v-if="!copied.dlist">Kopieren</span>
+                    <i v-else class="icon icon-check"></i>
+                  </button>
                 </div>
               </div>
-
+              <div class="columns mt-3" v-if="number > maxnumber">
+                <div class="column col-12">
+                  <p>Maximale Aufgabenzahl erreicht ({{ maxnumber }} Kombinationen möglich)</p>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -105,14 +127,69 @@ export default {
       min2: '',
       max2: '',
       number: '',
+      error: {
+        min1: false,
+        max1: false,
+        min2: false,
+        max2: false,
+        number: false,
+      },
+      maxnumber: '',
       mtasks: [],
       dtasks: [],
+      result: false,
+      copied: {
+        mlist: false,
+        dlist: false,
+      }
     }
   },
   methods: {
     generateTasks () {
-      this.mtasks = ['1','2','3']
-      this.dtasks = ['1','2','3']
+      // check input
+      this.error.min1 = (this.min1 === '') ? true : false
+      this.error.max1 = (this.max1 === '') ? true : false
+      this.error.min2 = (this.min2 === '') ? true : false
+      this.error.max2 = (this.max2 === '') ? true : false
+      this.error.number = (this.number === '') ? true : false
+      // no error occured: create result
+      if (!this.error.min1 && !this.error.max1 && !this.error.min2 && !this.error.max2 && !this.error.number) {
+        this.result = true
+        // init lists, integerify inputs
+        this.mtasks = [], this.dtasks = []
+        this.min1 = Number(this.min1), this.max1 = Number(this.max1), this.min2 = Number(this.min2), this.max2 = Number(this.max2)
+        // handle too high task number
+        this.maxnumber = (Math.abs(this.min1-this.max1)+1)*(Math.abs(this.min2-this.max2)+1)
+        if (this.number > this.maxnumber) {
+          this.number = this.maxnumber
+        }
+        var a, b, r, task
+        // generate multiplication tasks
+        while (this.mtasks.length < this.number) {
+          a = Math.floor(Math.random() * (this.max1 - this.min1 + 1)) + this.min1
+          b = Math.floor(Math.random() * (this.max2 - this.min2 + 1)) + this.min2
+          r = a * b
+          task =  a + ' • ' + b + ' = ' + r
+          if (!this.mtasks.includes(task)) {
+            this.mtasks.push(task)
+          }
+        }
+        // generate division tasks
+        while (this.dtasks.length < this.number) {
+          a = Math.floor(Math.random() * (this.max1 - this.min1 + 1)) + this.min1
+          b = Math.floor(Math.random() * (this.max2 - this.min2 + 1)) + this.min2
+          r = a * b
+          // check division by zero
+          if (b == 0) {
+            r = a
+            a = 'n.l.'
+          }
+          task =  r + ' : ' + b + ' = ' + a
+          if (!this.dtasks.includes(task)) {
+            this.dtasks.push(task)
+          }
+        }
+      }
     }
   }
 }
@@ -128,12 +205,19 @@ $primary-color: #f57c00;
 #app {
   margin-top: 60px;
 }
-
+ul {
+  list-style-type: none;
+  margin-left: 0;
+  font-family: "SF Mono", "Segoe UI Mono", "Roboto Mono", Menlo, Courier, monospace;
+  line-height: 1.2;
+}
 .mt-3 {
   margin-top: 1em;
 }
-
 .output {
-  min-height: 284px;
+  min-height: 296px;
+}
+.clip {
+  min-width: 100px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ClipboardJS from '../node_modules/clipboard/dist/clipboard.min.js'
 
 Vue.config.productionTip = false
+new ClipboardJS('.clip')
 
 new Vue({
   render: h => h(App),
